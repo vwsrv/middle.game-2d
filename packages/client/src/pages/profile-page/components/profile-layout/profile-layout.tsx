cebf94b97f7763b6de5514a8ca3876ecd220dfa3
Tamara Gudkova <goodluck.tamara@yandex.ru>
2025-08-21 18:46:52 +0300

    MID-30 add backgroud mixin

--- packages/client/src/pages/profile-page/components/profile-layout/profile-layout.tsx
+++ packages/client/src/pages/profile-page/components/profile-layout/profile-layout.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import './profile-layout.scss';
 
 const ProfileLayout = () => {
-  return <Outlet />;
+  return (
+    <div className="profile-layout-container">
+      <Outlet />
+    </div>
+  );
 };
 
 export default ProfileLayout;
