7a37e2de43bcbd44aaf13d3bb17474cd902841d9
Roslyakova Polina <czadma.rosliakova@globus.cz>
2025-08-22 15:37:50 +0300

    fix conflicts

--- packages/client/src/features/auth/api/login.ts
+++ packages/client/src/features/auth/api/login.ts
@@ -1,5 +1,6 @@
 import { AuthData, User } from '@/entities/user/model/types';
 
 export const login = async (data: AuthData): Promise<User> => {
+  console.log(data);
   return new Promise(resolve => resolve({} as User));
 };
